6a401e152c27baa0bfa58b951c853e2d7e0297db
Curtis Sommerlatte <curtis.sommerlatte@gmail.com>
2023-03-06 16:37:39 -0600

    start valid braces

--- random_practice.js
+++ random_practice.js
@@ -1,8 +1,34 @@
-console.log(isBalanced("What (is) this?") === true);
-console.log(isBalanced("What is) this?") === false);
-console.log(isBalanced("What (is this?") === false);
-console.log(isBalanced("((What) (is this))?") === true);
-console.log(isBalanced("((What)) (is this))?") === false);
-console.log(isBalanced("Hey!") === true);
-console.log(isBalanced(")Hey!(") === false);
-console.log(isBalanced("What ((is))) up(") === false);
+/*
+https://www.codewars.com/kata/5277c8a221e209d3f6000b56/train/javascript
+input: string
+output: boolean
+
+rules:
+- not balanced when:
+  a. if at any point there are more closing than opening
+  b. number of each is not equal
+  c. braces within a brace pair is not equal, e.g., '[(])'
+
+how to deal with c
+
+algo
+1. Create object of balances.
+2. Iterate through chars.
+   a. Update balances (switch):
+      - opening add
+      - closing subtract
+   b. If any of the balances object's values are negative, return false.
+   c. If ...
+3. Return every value of balances object is 0.
+
+*/
+
+function validBraces(str) {
+
+}
+
+console.log(validBraces("(){}[]")); // =>  True
+console.log(validBraces("([{}])")); // =>  True
+console.log(validBraces("(}")); //     =>  False
+console.log(validBraces("[(])")); //   =>  False
+console.log(validBraces("[({})](]")); //>  False
