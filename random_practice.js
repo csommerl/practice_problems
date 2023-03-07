d4e8cdff1470604c21d2e405f03f0e75ee9ffcfb
Curtis Sommerlatte <curtis.sommerlatte@gmail.com>
2023-03-07 09:47:28 -0600

    subfunctions

--- random_practice.js
+++ random_practice.js
@@ -10,6 +10,16 @@ rules:
   c. braces within a brace pair is not equal, e.g., '[(])'
 
 how to deal with c
+- for every brace with a matching end brace, the balances of the inner braces
+  equal 0
+- create strings of each??????
+
+Algo for c (seems inefficient)
+- test string as a whole for equal balances
+- for each opening brace:
+  - get substring through closing brace
+  - if no closing brace, return false
+  - otherwise, test braces' contents for equal balances
 
 algo
 1. Create object of balances.
@@ -24,7 +34,53 @@ algo
 */
 
 function validBraces(str) {
+  return checkBraceBalances(str);
+}
 
+function checkBraceBalances(str) {
+  const balances = {
+    parentheses: 0,
+    brackets: 0,
+    curlyBraces: 0,
+  };
+
+  for (let char of str) {
+    updateBraceBalances(char, balances);
+    if (hasNegValue(balances)) return false;
+  }
+
+  return !hasNonZeroValue(balances);
+}
+
+function updateBraceBalances(brace, braceBalances) {
+  switch (brace) {
+    case '(':
+      ++braceBalances.parentheses;
+      break;
+    case ')':
+      --braceBalances.parentheses;
+      break;
+    case '[':
+      ++braceBalances.brackets;
+      break;
+    case ']':
+      --braceBalances.brackets;
+      break;
+    case '{':
+      ++braceBalances.curlyBraces;
+      break;
+    case '}':
+      --braceBalances.curlyBraces;
+      break;
+  } 
+}
+
+function hasNegValue(obj) {
+  return Object.values(obj).some(value => value < 0);
+}
+
+function hasNonZeroValue(obj) {
+  return Object.values(obj).some(value => value !== 0);
 }
 
 console.log(validBraces("(){}[]")); // =>  True
