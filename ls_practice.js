a4ee3b530395e9ac260ce6ee80d3990d143af890
Curtis Sommerlatte <curtis.sommerlatte@gmail.com>
2023-03-10 10:09:18 -0600

    ...

--- ls_practice.js
+++ ls_practice.js
@@ -1,3 +1,1 @@
-leadingSubstrings('abc');      // ["a", "ab", "abc"]
-leadingSubstrings('a');        // ["a"]
-leadingSubstrings('xyzzy');    // ["x", "xy", "xyz", "xyzz", "xyzzy"]
+https://launchschool.com/exercises/3b0a4549
