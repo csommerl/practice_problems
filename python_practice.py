a7254575e5a1e396fa13d198cd3e870903c6d184
Curtis Sommerlatte <curtis.sommerlatte@gmail.com>
2023-04-05 10:02:34 -0500

    ...

--- python_practice.py
+++ python_practice.py
@@ -1,11 +1,4 @@
-def nb_year(pop, percent_growth, newly_moved, target):
-    years = 0
+def remove(str):
+    return str.rstrip('!')
 
-    while pop < target:
-        years += 1
-        pop += int((percent_growth / 100) * pop) + newly_moved
-
-    return years
-
-print(nb_year(1500, 5, 100, 5000)) # 15
-print(nb_year(1500000, 2.5, 10000, 2000000)) # 10
+print(remove('Hi!!!'))
