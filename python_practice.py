e8875838bd92e103e70173afdac44ba626d1e353
Curtis Sommerlatte <curtis.sommerlatte@gmail.com>
2023-05-24 13:57:47 -0500

    ...

--- python_practice.py
+++ python_practice.py
@@ -1,18 +1,60 @@
 """
+https://www.codewars.com/kata/57b06f90e298a7b53d000a86/train/python
+
+Algo
+1. Create time variable.
+2. Populate a new array `tills` of length num_of_tills
+    Order from smallest to largest.
+3. Find index of smallest num in array.
+    a. Add num to time.
+    b. Subtract from every ele in tills.
+    c. Place next customer in index.
+    d. Continue while
+        no new customers
+        some elements are non-zero
+
+helper functions
+elapse_time
+add_next_customer
 """
 
-def multiplication_table(size):
-    table = []
+def queue_time(customers, num_of_tills):
+    time = 0
+    tills = sorted([customers.pop(0) for i in range(num_of_tills)])
 
-    for i in range(1, size + 1):
-        row = []
+    while tills:
+        elapsed_time = tills.pop(0)
+        while tills and tills[0] == elapsed_time:
+            tills.pop()
+        time += elapsed_time
+        tills = [customer_time - elapsed_time for customer_time in tills]
 
-        for j in range(1, size + 1):
-            row.append(i * j)
+        if customers:
+            next_cust = customers.pop(0)
+            idx_of_next_largest = get_idx_of_next_largest(next_cust, customers)
+            if idx_of_next_largest >= 0:
+                tills.insert(idx_of_next_largest, next_cust)
+            else:
+                tills.append(next_cust)
 
-        table.append(row)
+    return time
 
-    return table
 
-print(multiplication_table(3))
-# [[1,2,3],[2,4,6],[3,6,9]]
+def get_idx_of_next_largest(num, lst):
+    for idx, ele in enumerate(lst):
+        if ele > num:
+            return idx
+    return -1
+
+
+# print(queue_time([5, 3, 4], 1)) # 12
+print(queue_time([10, 2, 3, 3], 2)) # 10
+# print(queue_time([2, 3, 10], 2)) # 12
+
+"""
+[2, 3, 10], 2
+till 1: 2
+till 2: 3
+
+find 
+"""
