7a89ef58218791d543936819f253feac2c4a7630
Curtis Sommerlatte <curtis.sommerlatte@gmail.com>
2023-05-24 16:00:46 -0500

    supermarket queue

--- python_practice.py
+++ python_practice.py
@@ -18,26 +18,46 @@ elapse_time
 add_next_customer
 """
 
+
 def queue_time(customers, num_of_tills):
     time = 0
-    tills = sorted([customers.pop(0) for i in range(num_of_tills)])
+
+    if not customers:
+        return time
+    
+    if len(customers) > num_of_tills:
+        tills = sorted([customers.pop(0) for i in range(num_of_tills)])
+    else:
+        tills = sorted([customers.pop(0) for cust in customers])
 
     while tills:
         elapsed_time = tills.pop(0)
-        while tills and tills[0] == elapsed_time:
-            tills.pop()
         time += elapsed_time
-        tills = [customer_time - elapsed_time for customer_time in tills]
 
-        if customers:
-            next_cust = customers.pop(0)
-            idx_of_next_largest = get_idx_of_next_largest(next_cust, customers)
-            if idx_of_next_largest >= 0:
-                tills.insert(idx_of_next_largest, next_cust)
-            else:
-                tills.append(next_cust)
+        tills = elapse_times(tills, elapsed_time)
+
+        if not customers:
+            continue
+
+        next_cust = customers.pop(0)
+        idx_of_next_largest = get_idx_of_next_largest(next_cust, tills)
+
+        if idx_of_next_largest >= 0:
+            tills.insert(idx_of_next_largest, next_cust)
+        else:
+            tills.append(next_cust)
 
     return time
+
+
+def elapse_times(tills, time):
+    new_tills = []
+    for till in tills:
+        till -= time
+        if till <= 0:
+            continue
+        new_tills.append(till)
+    return new_tills
 
 
 def get_idx_of_next_largest(num, lst):
@@ -47,14 +67,50 @@ def get_idx_of_next_largest(num, lst):
     return -1
 
 
-# print(queue_time([5, 3, 4], 1)) # 12
-print(queue_time([10, 2, 3, 3], 2)) # 10
-# print(queue_time([2, 3, 10], 2)) # 12
+#print(queue_time([5, 3, 4], 1)) # 12
+#print(queue_time([10, 2, 3, 3], 2)) # 10
+#print(queue_time([2, 3, 10], 2)) # 12
+#print(queue_time([], 1)) # 0
+#print(queue_time([2], 5)) # 2
+
+print(queue_time([1,2,3,4,5], 100)) # 5: time should be the longest in customers
+print(queue_time([2,2,3,3,4,4], 2)) # 9: what to do when all tills go to zero
+
+# print(queue_time([9, 46, 48, 29, 39, 5, 40, 17, 10, 26, 33, 19, 28, 21], 4)) # 102
 
 """
-[2, 3, 10], 2
-till 1: 2
-till 2: 3
+[10, 2, 3, 3], 2
 
-find 
+customers: [3, 3]
+time: 0
+tills: [2, 10]
+
+customers: [3, 3]
+time: 2
+tills: [8]
+
+#####
+customers: [3]
+time: 2
+tills: [3, 8]
+
+customers: [3]
+time: 5
+tills: [5]
+
+customers: []
+time: 5
+tills: [3, 5]
+
+customers: []
+time: 8
+tills: [2]
+
+customers: []
+time: 8
+tills: [2]
+
+customers: []
+time: 10
+tills: []
 """
